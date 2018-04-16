bd44b437f1cc24de7e68e049b6a87fa40142fbed
WeiCheng <bravo325806@gmail.com>
2018-04-16 15:15:30 +0800

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "password";
 $dbname = "dbname"
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+$conn = new mysqli_connect($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
