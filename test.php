0cda08dce5424c567bdf14703e18037d15de7525
WeiCheng <bravo325806@gmail.com>
2018-04-16 15:43:25 +0800

    Update test.php

--- test.php
+++ test.php
@@ -2,24 +2,23 @@
 $servername = "mysql";
 $username = "root";
 $password = "password";
-$dbname = "dbname"
+$dbname = "dbname";
 // Create connection
-$conn = new mysqli_connect($servername, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo $conn;
-//$sql = "SELECT * FROM tablename";
-//$result = mysqli_query($conn, $sql);
-//if (mysqli_num_rows($result) > 0) {
-//    // output data of each row
-//    while($row =mysqli_fetch_array($result)) {
-//        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
-//    }
-//} else {
-//    echo "0 results";
-//}
-//$conn->close();
+$sql = "SELECT * FROM tablename";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+   // output data of each row
+    while($row =mysqli_fetch_array($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
