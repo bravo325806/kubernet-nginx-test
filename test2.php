33e636fdc0c56ecf0aa548bd85e593baa780cce6
bravo325806 <bravo325806@gmail.com>
2018-04-20 08:55:39 +0800

    add test2

---
+++ test2.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "mysql-0.mysql";
+$username = "root";
+$password = "";
+$dbname = "dbname";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT * FROM tablename";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+   // output data of each row
+    while($row =mysqli_fetch_array($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
